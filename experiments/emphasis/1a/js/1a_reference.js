1d57b75b395f0d2945f1cc1d76a366f06efb8dba
noaattali <59579548+noaattali@users.noreply.github.com>
2020-04-06 14:59:20 -0700

    updates to exp1a

--- experiments/emphasis/1a/js/1a_reference.js
+++ experiments/emphasis/1a/js/1a_reference.js
@@ -108,8 +108,8 @@ function make_slides(f) {
         gender : $("#gender").val(),
         education : $("#education").val(),
         comments : $("#comments").val(),
-        problems: $("#problems").val(),
-        fairprice: $("#fairprice").val()
+        // problems: $("#problems").val(),
+        // fairprice: $("#fairprice").val()
       };
       exp.go(); //use exp.go() if and only if there is no "present" data.
     }
