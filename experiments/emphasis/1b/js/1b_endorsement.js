a7c98aef8dfaf71b4e60ec86e8b61f4a53f64ca1
noaattali <59579548+noaattali@users.noreply.github.com>
2020-04-27 13:50:38 -0700

    simplified exp1b and added exp1a graph of some results

--- experiments/emphasis/1b/js/1b_endorsement.js
+++ experiments/emphasis/1b/js/1b_endorsement.js
@@ -34,26 +34,16 @@ function make_slides(f) {
     present : [{
           amb: {unemphasized: {every: "Every marble isn't red.", some: "Some marble isn't red.", one: "One marble isn't red.", no: "No marble isn't red."}, emphasized: {every: "<em><strong>EVERY</strong></em> marble isn't red.", some: "<em><strong>SOME</strong></em> marble isn't red.", one: "<em><strong>ONE</strong></em> marble isn't red.", no: "<em><strong>NO</strong></em> marble isn't red."}},
           paraphrase: {surface: {every: "None of the marbles are red.", some: "Not all of the marbles are red.", one: "There's a marble that isn't red.", no: "All of the marbles are red."}, inverse: {every: "Not all of the marbles are red.", some: "None of the marbles are red.", one: "It's not the case that there's a marble that's red.", no: "Some of the marbles are red."}},
-          contexts: {exact: "Bluesy bet Rose on <strong>the exact number of marbles that are red</strong>: Bluesy wins if 0 or 2 marbles are red, and Rose wins if 1 or 3 marbles are red.", all: "Bluesy bet Rose on <strong>whether or not each of the marbles are red</strong>: Bluesy wins if 3 marbles are red, and Rose wins if 0, 1, or 2 marbles are red.", none: "Bluesy bet Rose on <strong>whether or not none of the marbles are red</strong>: Bluesy wins if 0 marbles are red, and Rose wins if 1, 2, or 3 marbles are red.", one: "Bluesy bet Rose on <strong>whether or not just one of the marbles is red</strong>: Bluesy wins if 1 marble is red, and Rose wins if 0, 2, or 3 marbles are red."},
-          QUDs: {exact: "We bet on the number of marbles that are red.", all: "We bet on whether the number of marbles that are red is 3.", none: "We bet on whether the number of marbles that are red is 0.", one: "We bet on whether the number of marbles that are red is 1."},
-          visuals: {exact: "<img src = \"../1b/expt-files/images/howQUD.png\" width=\"400\">",
-          all: "<img src = \"../1b/expt-files/images/allQUD.png\" width=\"400\">",
-          none: "<img src = \"../1b/expt-files/images/noneQUD.png\" width=\"400\">",
-          one: "<img src = \"../1b/expt-files/images/oneQUD.png\" width=\"400\">"},
         }],
 
       present_handle : function(stim) {
         $(".err").hide();
-        progress();
 
         this.stim = stim;
         exp.emph = _.sample(["unemphasized","emphasized"])
         exp.quantifier = _.sample(["every","some","one","no"]);
         exp.QUD = _.sample(["exact", "all", "none", "one"]);
 
-        $(".context").html(stim["contexts"][exp.QUD]);
-        $(".visual").html(stim["visuals"][exp.QUD]);
-        $(".QUD").html("Bluesy told Mellow: "+stim["QUDs"][exp.QUD]);
         $(".amb").html("\""+stim["amb"][exp.emph][exp.quantifier]+"\"");
 
         this.sentence_types = _.shuffle(["surface","inverse"]);
